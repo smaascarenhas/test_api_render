0a534479d87d4300e36781c26d651f94e1a38fc1
Samir <smascarenhas313@gmail.com>
2024-07-14 14:23:40 -0300

    updated handler

--- handler.py
+++ handler.py
@@ -45,4 +45,4 @@ def rossmann_predict():
         return Response( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    app.run( '0.0.0.0' )
+    app.run( '0.0.0.0', port=5000 )
